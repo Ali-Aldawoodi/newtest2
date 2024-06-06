5a069d6d20b6b9abc4d285085ee8ecd58f05c56a
Ali Aldawoodi <alialdawoodi99@yahoo.com>
2024-06-06 12:42:29 -0600

    saved

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-asdfasdfasdfsadfasdf
+asdfasdfasdfsadfasdfasdfasdfsdafasdfasdfsa
+
+function yes () {
+    console.log("successgit st")
+}adfasdf
+
+dfasd
+asdfasdfasdfsadfasdfasdfasdfsdafasdfasdfsa
+asdfasdf
